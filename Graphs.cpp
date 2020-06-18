567a536eaa14739cfa839b92f946dfed7d01b4cb
justvg <georgevarvashtyan@gmail.com>
2020-06-18 15:28:38 +0300

    Kruskal, Prim

--- Graphs.cpp
+++ Graphs.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #define ArrayCount(Array) (sizeof(Array)/sizeof((Array)[0]))
 #define Assert(Expression) if(!(Expression)) { *(int *)0 = 0; } 
@@ -209,6 +210,165 @@ DFS(graph* Graph)
 	}
 }
 
+
+//
+// NOTE(georgy): Minimum Spanning Trees. Kruskal's, Prim's algorithms
+//
+
+// NOTE(georgy): I use this for Kruskal's and Prim's algorithms
+struct edge
+{
+	uint32_t FirstVertex, SecondVertex;
+	uint32_t Weight;
+};
+
+static void
+MergeSort(uint32_t Count, edge *Edges, edge* TempStorage)
+{
+	if (Count == 1)
+	{
+		return;
+	}
+	else if (Count == 2)
+	{
+		if (Edges[0].Weight > Edges[1].Weight)
+		{
+			edge Temp = Edges[0];
+			Edges[0] = Edges[1];
+			Edges[1] = Temp;
+		}
+	}
+	else
+	{
+		uint32_t CountHalf0 = Count / 2;
+		uint32_t CountHalf1 = Count - CountHalf0;
+
+		edge* Half0 = Edges;
+		edge* Half1 = Edges + CountHalf0;
+		MergeSort(CountHalf0, Half0, TempStorage);
+		MergeSort(CountHalf1, Half1, TempStorage);
+
+		edge* ReadHalf0 = Half0;
+		edge* ReadHalf1 = Half1;
+		edge* End = Edges + Count;
+
+		edge* Out = TempStorage;
+		for (uint32_t I = 0; I < Count; I++)
+		{
+			if (ReadHalf0 == Half1)
+			{
+				*Out++ = *ReadHalf1++;
+			}
+			else if (ReadHalf1 == End)
+			{
+				*Out++ = *ReadHalf0++;
+			}
+			else if (ReadHalf0->Weight < ReadHalf1->Weight)
+			{
+				*Out++ = *ReadHalf0++;
+			}
+			else
+			{
+				*Out++ = *ReadHalf1++;
+			}
+		}
+
+		for (int I = 0; I < Count; I++)
+		{
+			Edges[I] = TempStorage[I];
+		}
+	}
+}
+
+static std::vector<edge>
+MSTKruskal(graph *Graph, std::vector<edge> &Edges)
+{
+	std::vector<edge> Result;
+
+	int* ComponentsPerVertex = (int*)malloc(Graph->VertexCount*sizeof(int));
+	if (ComponentsPerVertex)
+	{
+		for (int I = 0; I < Graph->VertexCount; I++)
+		{
+			ComponentsPerVertex[I] = Graph->Vertices[I].Index;
+		}
+
+		edge* TempStorage = (edge*)malloc(Edges.size()*sizeof(edge));
+		MergeSort(Edges.size(), &Edges[0], TempStorage);
+
+		for (edge& Edge : Edges)
+		{
+			int FirstVertexComponent = ComponentsPerVertex[Edge.FirstVertex - 1];
+			int SecondVertexComponent = ComponentsPerVertex[Edge.SecondVertex - 1];
+			if (FirstVertexComponent != SecondVertexComponent)
+			{
+				for (int I = 0; I < Graph->VertexCount; I++)
+				{
+					if (ComponentsPerVertex[I] == SecondVertexComponent)
+					{
+						ComponentsPerVertex[I] = FirstVertexComponent;
+					}
+				}
+				
+				Result.push_back(Edge);
+			}
+		}
+	}
+
+	return(Result);
+}
+
+static bool
+Contains(std::vector<uint32_t> &Indices, uint32_t Check)
+{
+	bool Result = (std::find(Indices.begin(), Indices.end(), Check) != Indices.end());
+	return(Result);
+}
+
+static std::vector<edge>
+MSTPrim(graph* Graph, std::vector<edge> &Edges)
+{
+	std::vector<edge> Result;
+
+	std::vector<uint32_t> MSTVertices;
+	MSTVertices.push_back(1);
+	
+	while (MSTVertices.size() != Graph->VertexCount)
+	{
+		uint32_t MinWeight = UINT32_MAX;
+		edge* MinEdge = 0;
+		uint32_t VertexToAdd = 0;
+		for (edge& Edge : Edges)
+		{
+			bool ContainsFirstVertex = Contains(MSTVertices, Edge.FirstVertex);
+			bool ContainsSecondVertex = Contains(MSTVertices, Edge.SecondVertex);
+			if (ContainsFirstVertex && !ContainsSecondVertex)
+			{
+				if (Edge.Weight < MinWeight)
+				{
+					MinWeight = Edge.Weight;
+					MinEdge = &Edge; 
+					VertexToAdd = Edge.SecondVertex;
+				}
+			}
+			else if (!ContainsFirstVertex && ContainsSecondVertex)
+			{
+				if (Edge.Weight < MinWeight)
+				{
+					MinWeight = Edge.Weight;
+					MinEdge = &Edge;
+					VertexToAdd = Edge.FirstVertex;
+				}
+			}
+		}
+
+		MSTVertices.push_back(VertexToAdd);
+		Result.push_back(*MinEdge);
+	}
+
+	return(Result);
+}
+
 int main(void)
 {
 	//
@@ -232,6 +392,7 @@ int main(void)
 	AddNeighbourToVertex(&Graph0, 7, 8);
 
 	BFS(&Graph0, 1);
+	std::cout << "Path from 1 to 8:" << std::endl;
 	PrintPath(&Graph0, 1, 8);
 	std::cout << std::endl;
 
@@ -252,6 +413,35 @@ int main(void)
 	AddNeighbourToVertex(&Graph1, 6, 6);
 
 	DFS(&Graph1);
+
+
+	//
+	// NOTE(georgy): Minimum Spanning Trees. Kruskal's, Prim's algorithms
+	//
+	graph Graph2 = {};
+	for (int I = 0; I < 9; I++)
+	{
+		AddVertex(&Graph2);
+	}
+
+	std::vector<edge> Edges;
+	Edges.push_back({ 1, 2, 4 });
+	Edges.push_back({ 1, 8, 8 });
+	Edges.push_back({ 2, 3, 8 });
+	Edges.push_back({ 2, 8, 11 });
+	Edges.push_back({ 3, 4, 7 });
+	Edges.push_back({ 3, 6, 4 });
+	Edges.push_back({ 3, 9, 2 });
+	Edges.push_back({ 4, 5, 9 });
+	Edges.push_back({ 4, 6, 14 });
+	Edges.push_back({ 5, 6, 10 });
+	Edges.push_back({ 6, 7, 2 });
+	Edges.push_back({ 7, 8, 1 });
+	Edges.push_back({ 7, 9, 6 });
+	Edges.push_back({ 8, 9, 7 });
+
+	std::vector<edge> MST0 = MSTKruskal(&Graph2, Edges);
+	std::vector<edge> MST1 = MSTPrim(&Graph2, Edges);
 
 	return(0);
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This repository contains my implementation of some of the algorithms from "Introduction to Algorithms" book.<br/>
+This repository contains my implementation of some of the algorithms from "Introduction to Algorithms" book. I know that these algorithms can be more efficient, but the main goal is not to write the most efficient ones but just to get familiar with them.<br/>
 <br/>
 
 At the moment file Sorts.cpp contains implementations of: InsertionSort, SelectionSort, MergeSort and BinarySearch; binary_heap, max-priority queue, Heap Sort; QuickSort, RandomizedQuickSort, HoareQuickSort; CountingSort, RadixSort, BucketSort. <br/>
@@ -7,5 +7,5 @@ At the moment file Sorts.cpp contains implementations of: InsertionSort, Selecti
 At the moment file DataStructures.cpp contains implementations of:  Stack, Queue, Linked List; Binary Tree, Tree; Chained Hash Table, Open Addressing Hash Table; Binary Search Tree; some of Red-Black Tree<br/>
 <br/>
 
-At the moment file Graphs.cpp contains implementations of:  Graph, BFS, DFS<br/>
+At the moment file Graphs.cpp contains implementations of:  Graph, BFS, DFS; Minimum Spanning Tree, Kruskal's and Prim's algorithms.<br/>
 <br/>
